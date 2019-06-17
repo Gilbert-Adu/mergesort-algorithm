19925264a899bc7f16af5abab9e5334e887001fa
Gilbert Adu <50780928+Gilbert-Adu@users.noreply.github.com>
2019-06-16 22:06:56 -0400

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,1 +1,38 @@
-
+def mergesort(array):
+  if len(array) > 1:
+    mid = len(array)//2
+    leftarr = array[:mid]
+    rightarr = array[mid:]
+    mergesort(leftarr)
+    mergesort(rightarr)
+    
+    i = 0
+    j = 0
+    k = 0
+    while i < len(leftarr) and j < len(rightarr):
+    
+      if leftarr[i] < rightarr[j]:
+        array[k] = leftarr[i]
+        i += 1
+      else:
+        array[k] = rightarr[j]
+        j += 1
+      
+      
+      k +=1
+      
+    while i < len(leftarr):
+    
+       array[k] = leftarr[i]
+       i += 1
+       k += 1
+      
+    while j < len(rightarr):
+       array[k] = rightarr[j]]
+       j += 1
+       k += 1
+        
+    return array
+      
+   
+    
